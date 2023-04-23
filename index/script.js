070decac9eb262ff50f22a66697f30f6925fc047
Vaibhav Kaushik <vaibhavsingh5373@gmail.com>
2023-04-24 00:11:06 +0530

    inital comit

---
+++ index/script.js
@@ -0,0 +1,10 @@
+const option1Button = document.getElementById('option1');
+const option2Button = document.getElementById('option2');
+
+option1Button.addEventListener('click', () => {
+	alert('You chose Option 1.');
+});
+
+option2Button.addEventListener('click', () => {
+	alert('You chose Option 2.');
+});
